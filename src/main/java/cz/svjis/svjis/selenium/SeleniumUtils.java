ceb4b9a0018a7bc0895680da0ffd1dad7a996bed
Jarda Beran <jaroslav.beran@gmail.com>
2023-01-24 19:17:50 +0100

    Feature/small changes (#1)
    * Maven - no transfer progress
    
    * Window size

--- src/main/java/cz/svjis/svjis/selenium/SeleniumUtils.java
+++ src/main/java/cz/svjis/svjis/selenium/SeleniumUtils.java
@@ -63,7 +63,7 @@ public class SeleniumUtils {
             System.setProperty("webdriver.chrome.driver", path);
         } else {
             options.addArguments("--headless");
-            options.addArguments("window-size=1400,2100");
+            options.addArguments("window-size=1200,1200");
         }
         driver = new ChromeDriver(options);
         return driver;
